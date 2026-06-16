9af20b69e89abb32190a0cb04add899342124784
Subhadip-Paul2006 <subhadippaulff@gmail.com>
2026-06-16 06:22:25 +0530

    New folder

---
+++ Contests/CoedCheif/NQuennsProblem.cpp
@@ -0,0 +1,23 @@
+/* Problem Statement :
+You are given a chessboard of dimensions N × N. Your task is to place N queens on the chessboard such that no two queens attack each other. A queen can attack another piece if they are positioned in the same row, column, or diagonal. You need to find all distinct valid configurations of the chessboard.
+
+Function Name : solveNQueens – This function finds all unique valid placements of N queens on an N × N chessboard.
+Parameters : N (integer) – The size of the chessboard and the number of queens to place.
+Return Value : A vector of vectors of strings, where each inner vector represents a valid configuration of the chessboard. Each string in the inner vector represents a row of the chessboard, with 'Q' indicating a queen and '.' indicating an empty space. */
+
+/* Input Format : 
+A single line containing an integer N, representing the size of the chessboard.
+Output Format : 
+The output consists of all distinct valid configurations of the chessboard, each represented as a vector of strings.
+Constraints : 
+1 ≤ N ≤ 9 */
+
+
+
+
+
+
+
+
+
+
