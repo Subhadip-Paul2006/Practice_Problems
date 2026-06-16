89558db62df7daf4d8b6d38075cac713d0db0130
Subhadip-Paul2006 <subhadippaulff@gmail.com>
2026-06-16 07:13:27 +0530

    New folder

---
+++ Contests/CoedCheif/HistogramLargestRectangle.py
@@ -0,0 +1,36 @@
+# Problem Statement :
+# You are given an array A of N positive integers representing the heights of bars in a histogram. Each bar has a width of 1 unit. Your task is to find the area of the largest rectangle that can be formed within the bounds of the histogram.
+# A1, A2, A3, ..., AN. 
+# A represents a histogram which means the height of the i-th bar will be Ai units and the width of each bar is 1 unit.
+# Find the area of the largest rectangle in the histogram.
+
+# Function Declaration
+# Function Name :
+# largestRectangleArea – This function calculates the maximum rectangular area possible within the given histogram.
+
+# Parameters :
+# N : An integer representing the number of bars in the histogram.
+# A : An array of integers representing the heights of the histogram bars.
+# Return Value
+# Returns an integer: the area of the largest rectangle that can be formed in the histogram.
+
+# Constraints:
+# 1 ≤ T ≤ 10
+# 1 ≤ N ≤ 10^5
+# 0 ≤ Ai ≤ 10^9 
+# The input and output formats provided below are only for testing with custom inputs. You only need to return the value. Printing is handled automatically.
+
+# Input Format
+# 01. The first line of input will contain a single integer T, denoting the number of test cases. 
+# 02. The first line of each test case contains an integer N - the length of the array A
+# 03. The second line of each test case contains  N space-separated integers A1, A2, ..., AN - the heights of the histogram bars.
+
+# Output Format
+# For each test case, output on a new line the area of the largest rectangle in the histogram.
+
+# Subtasks
+# Subtask 1 (30 points): 1 ≤ N ≤ 1000
+# Subtask 2 (70 points): No further constraints.
+
+
+
