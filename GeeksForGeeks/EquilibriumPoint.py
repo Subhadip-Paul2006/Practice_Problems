6efa9450cc28cd4030b55b2515a9c304c6a035c9
Subhadip-Paul2006 <subhadippaulff@gmail.com>
2026-07-25 07:10:29 +0530

    GFG question

---
+++ GeeksForGeeks/EquilibriumPoint.py
@@ -0,0 +1,13 @@
+# Given an array of integers arr[], the task is to find the first equilibrium point in the array.
+# The equilibrium point in an array is an index (0-based indexing) such that the sum of all elements before that index is the same as the sum of elements after it. Return -1 if no such point exists. 
+
+# Examples:
+# Input: arr[] = [1, 2, 0, 3]
+# Output: 2 
+
+# Input: arr[] = [1, 1, 1, 1]
+# Output: -1
+
+# Input: arr[] = [-7, 1, 5, 2, -4, 3, 0]
+# Output: 3
+
